fe19327ff83aa1159255b732d09c3f6c2b51773f
Lenilson Soares <lenilsonp40@gmail.com>
2023-10-21 17:18:14 -0300

    sintaxe

---
+++ Fundamentos/Program.cs
@@ -0,0 +1,11 @@
+﻿using Fundamentos.Models;
+
+Pessoa p = new Pessoa();
+
+p.Nome =  "Butta";
+p.Idade = 26;
+p.Apresentar();
+
+DateTime dataatual = DateTime.Now;
+Console.WriteLine(dataatual.AddDays(5));
+
