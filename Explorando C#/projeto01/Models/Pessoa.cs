9fde377ae9baf77226d1cf5d56249e82b35e67f5
Lenilson Soares <lenilsonp40@gmail.com>
2023-11-03 16:13:51 -0300

    pessoa

--- Explorando C#/projeto01/Models/Pessoa.cs
+++ Explorando C#/projeto01/Models/Pessoa.cs
@@ -8,11 +8,13 @@ namespace projeto01.Models
     public class Pessoa
     {
         public string Nome { get; set; }
+        public string Sobrenome { get; set;}        
+        public string NomeCompleto => $"{Nome} {Sobrenome}";
         public int Idade { get; set; }
 
         public void Apresentar()
         {
-            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");                        
+            Console.WriteLine($"Nome: {NomeCompleto}, Idade: {Idade}");                        
         }
     }
 }
--- Explorando C#/projeto01/Program.cs
+++ Explorando C#/projeto01/Program.cs
@@ -3,5 +3,6 @@
 
 Pessoa p1 = new Pessoa();
 p1.Nome = "Lenilson";
+p1.Sobrenome = "Soares"
 p1.Idade = 32;
 p1.Apresentar();
