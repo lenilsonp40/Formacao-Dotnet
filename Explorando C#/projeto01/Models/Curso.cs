45e464ff35f1b66c94b25e2215087549b9515246
Lenilson Soares <lenilsonp40@gmail.com>
2023-11-03 17:36:44 -0300

    alunos

---
+++ Explorando C#/projeto01/Models/Curso.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto01.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno(Pessoa aluno)
+        {
+            Alunos.Add(aluno);
+        }
+
+        public int ObterQuantidadeAlunosMatriculados()
+        {
+            int quantidade = Alunos.Count;
+            return quantidade;
+        }
+
+        public bool RemoverAluno(Pessoa aluno)
+        {
+            return Alunos.Remove(aluno);
+        }
+
+        public void ListarAlunos()
+        {
+            foreach (Pessoa aluno in Alunos)
+            {
+                Console.WriteLine(aluno.NomeCompleto);
+            }
+        }
+    }
+}
+
--- Explorando C#/projeto01/Program.cs
+++ Explorando C#/projeto01/Program.cs
@@ -1,8 +1,21 @@
 ﻿using projeto01.Models;
 
 
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Lenilson";
+p1.Sobrenome = "Soares"
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+
+
+/*
 Pessoa p1 = new Pessoa();
 p1.Nome = "Lenilson";
 p1.Sobrenome = "Soares"
 p1.Idade = 32;
 p1.Apresentar();
+*/
