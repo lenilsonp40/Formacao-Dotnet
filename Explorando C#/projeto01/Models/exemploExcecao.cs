546b0ae9c03b279d71b15de57f45908be3ae04d9
Lenilson Soares <lenilsonp40@gmail.com>
2024-04-03 18:19:56 -0300

    throw

---
+++ Explorando C#/projeto01/Models/exemploExcecao.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto01.Models
+{
+    public class exemploExcecao
+    {
+        public void Metodo1()
+        {
+            
+        }
+    }
+}
--- Explorando C#/projeto01/Program.cs
+++ Explorando C#/projeto01/Program.cs
@@ -2,19 +2,22 @@
 using System.Globalization;
 
 
+
+
+
 // parei validaçoes get e set
 
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Lenilson";
-p1.Sobrenome = "Soares";
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Lenilson";
+// p1.Sobrenome = "Soares";
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Inglês";
-cursoDeIngles.Alunos = new List<Pessoa>();
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Inglês";
+// cursoDeIngles.Alunos = new List<Pessoa>();
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.ListarAlunos();
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.ListarAlunos();
 
 
 
@@ -30,18 +33,24 @@ p1.Apresentar();
 
 
 
+// try {
 
-/*
-string[] linhas = file.ReadAllLines("Arquivos/arquivosLeitura.txt");
+// string[] linhas = File.ReadAllLines("Arquivos/arquivos_Leitura.txt");
 
-foreach(string linha in linhas)
-{
-    Console.WriteLine(linha);
-}
+// foreach(string linha in linhas)
+// {
+//     Console.WriteLine(linha);
+// }
+
+// } catch(Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+// }
 
 
 
 
 
 
-*/
+
+
